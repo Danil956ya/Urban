5f854325ad307ac5614d77010e18e6fb0ccbde80
Danil <91548049+Danil956ya@users.noreply.github.com>
2024-12-07 18:06:43 +0300

    Create 2.1.py
    Задача "Матрица воплоти":
    
    Напишите функцию get_matrix с тремя параметрами n, m и value, которая будет создавать матрицу(вложенный список) размерами n строк и m столбцов, заполненную значениями value и возвращать эту матрицу в качестве результата работы.
    
    Пункты задачи:
    
    Объявите функцию get_matrix и напишите в ней параметры n, m и value.
    Создайте пустой список matrix внутри функции get_matrix.
    Напишите первый(внешний) цикл for для кол-ва строк матрицы, n повторов.
    В первом цикле добавляйте пустой список в список matrix.
    Напишите второй(внутренний) цикл for для кол-ва столбцов матрицы, m повторов.
    Во втором цикле пополняйте ранее добавленный пустой список значениями value.
    После всех циклов верните значение переменной matrix.
    Выведите на экран(консоль) результат работы функции get_matrix.
    Пример результата выполнения функции:

---
+++ 2.1.py
@@ -0,0 +1,18 @@
+# Задача "Матрица воплоти":
+def get_matrix(n,m,value):
+    matrix = []
+
+    for repeats in range(n):
+        row = []
+        for repeats in range(m):
+            row.append(value)
+        matrix.append(row)
+    return matrix
+
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+
+print(result1)
+print(result2)
+print(result3)
