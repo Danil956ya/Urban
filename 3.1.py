ae55ecaa026d03143a143d0678bdfc32e68d6447
Danil <91548049+Danil956ya@users.noreply.github.com>
2024-12-11 12:43:45 +0300

    Update 3.1.py

--- 3.1.py
+++ 3.1.py
@@ -9,3 +9,13 @@ player_stat = {
 with open("PlayerSave.txt", "w") as file:
     for el in player_stat.items():
         file.write(el[0] + ":" + str(el[1]) + "\n")
+
+with open("PlayerSave.txt", "r") as file:
+    stat_str = file.read()
+    print(stat_str)
+
+player_stat["IsJumping"] = False
+new_key_value = f"IsJumping:{player_stat['IsJumping']}\n"
+
+with open("PlayerSave.txt", "a") as file:
+    file.write(new_key_value)
