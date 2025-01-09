5e474e1c90e1bb353af2e33466eacfb339ac54b0
Danil <91548049+Danil956ya@users.noreply.github.com>
2025-01-09 15:18:06 +0300

    Add files via upload

---
+++ 4.5.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.common.by import By
+
+def test_login_redirect():
+    correct_url = 'https://www.saucedemo.com/inventory.html'
+    get_url = driver.current_url
+    assert correct_url == get_url, "Не прошли на страничку"
+
+driver = webdriver.Chrome()
+driver.get("http://www.saucedemo.com/")
+driver.maximize_window()
+
+username_field = driver.find_element(By.XPATH, '//input[@id="user-name"]')
+password_field = driver.find_element(By.XPATH, '//input[@id="password"]')
+login_button =  driver.find_element(By.XPATH, '//input[@id="login-button"]')
+
+login = "standard_user"
+username_field.send_keys(login)
+
+password = "secret_sauce"
+password_field.send_keys(password)
+
+login_button.click()
+
+sleep(5)
+
+test_login_redirect()
